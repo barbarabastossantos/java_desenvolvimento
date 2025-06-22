a8ea65427961f982410b5d643ae48b7eb7a992a4
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-06-22 19:20:12 -0300

    aprendendo java

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/barbara/desenvolvendojava/DesenvolvendoJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package barbara.desenvolvendojava;
+
+/**
+ *
+ * @author ADM
+ */
+public class DesenvolvendoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
