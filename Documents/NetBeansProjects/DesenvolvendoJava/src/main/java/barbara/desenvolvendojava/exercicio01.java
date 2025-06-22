37d14f1823ed542c63af7bda3c2b38f68ad9d21a
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-06-22 20:47:12 -0300

    exercicio de precedencia com exemplo de calculo de area

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/barbara/desenvolvendojava/exercicio01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package barbara.desenvolvendojava;
+
+/**
+ *
+ * @author ADM
+ */
+public class exercicio01 {
+    public static void main(String[] args) {
+        // exercicio de calculo de area 
+        int bmaior=8;
+        int bmenor=6;
+        int altura=5;
+        
+        int area= ((bmenor+bmaior)/2)*altura;
+        System.out.println(area);
+    }
+    
+}
