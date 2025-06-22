44583af43c17145c0ae652927c60704c23b51f6a
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-06-22 20:34:37 -0300

    aprendendo operadores aritmeticos basicos

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/barbara/desenvolvendojava/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package barbara.desenvolvendojava;
+
+/**
+ *
+ * @author ADM
+ */
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int y=12;
+        int x=45;
+        int b=34;
+        int m=9;
+        
+        System.out.println(x-y);
+        System.out.println(b+m);
+        System.out.println(y*m);
+        System.out.println(x/m);
+    }
+    
+}
