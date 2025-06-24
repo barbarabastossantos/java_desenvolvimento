5737ada4931498fafe3ef55d435ef4e6f225100b
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-06-24 15:24:53 -0300

    entrada de dados Scanner

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/barbara/desenvolvendojava/Entrada01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package barbara.desenvolvendojava;
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author ADM
+ */
+public class Entrada01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x;
+        x=sc.next();
+        System.out.println("Você digitou : " + x);
+        
+        sc.close();
+    }
+    
+}
