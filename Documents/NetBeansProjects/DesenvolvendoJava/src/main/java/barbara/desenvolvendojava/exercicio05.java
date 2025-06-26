1791d457673def204225e3bf666c206b983760b7
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-06-26 19:53:27 -0300

    exercicio com scanner

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/barbara/desenvolvendojava/exercicio05.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package barbara.desenvolvendojava;
+
+import java.util.Scanner;
+/**
+ *
+ * @author ADM
+ */
+public class exercicio05 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        
+        int x= sc.nextInt();
+        int y= sc.nextInt();
+        
+        int soma=x+y;
+        
+        System.out.println(" soma : " + soma);
+        
+        sc.close();
+        
+    }
+    
+}
