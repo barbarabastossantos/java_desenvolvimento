43070a2cd930332ca2fccb7a7f52ed52bdd98038
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-06-29 22:29:07 -0300

    condicional simples

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/estrutura_condicional/exercicio01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estrutura_condicional;
+
+/**
+ *
+ * @author ADM
+ */
+public class exercicio01 {
+    public static void main(String[] args) {
+        int x=5;
+        
+       if (x<3) {
+          System.out.println("Bom Dia");
+       }
+        System.out.println("Boa Tarde");
+        System.out.println("Boa Noite");
+    }
+    
+}
