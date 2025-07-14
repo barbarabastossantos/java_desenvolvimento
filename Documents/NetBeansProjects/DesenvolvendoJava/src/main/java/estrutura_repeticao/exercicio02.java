88b97585818d295ebf7834f0da4c72316c393cf5
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-07-14 06:10:43 -0300

    estrutura de repeticao

---
+++ Documents/NetBeansProjects/DesenvolvendoJava/src/main/java/estrutura_repeticao/exercicio02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estrutura_repeticao;
+
+
+/**
+ *
+ * @author ADM
+ */
+public class exercicio02 {
+    public static void main(String[] args) {
+        int contador=0;
+        
+        while(contador<10){
+            contador=contador+1;
+            System.out.println(contador);
+        }
+    }
+ 
+}
