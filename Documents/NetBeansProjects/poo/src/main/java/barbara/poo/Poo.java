aa5f8c5ea2aed24c783630e37a7cc4cff4b3db14
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-07-25 19:18:38 -0300

    programa com poo

---
+++ Documents/NetBeansProjects/poo/src/main/java/barbara/poo/Poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package barbara.poo;
+
+/**
+ *
+ * @author ADM
+ */
+public class Poo {
+
+    public static void main(String[] args) {
+        System.out.println("Vamos de POO!");
+    }
+}
