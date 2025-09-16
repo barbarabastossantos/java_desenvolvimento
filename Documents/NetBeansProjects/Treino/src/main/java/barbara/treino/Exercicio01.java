dff3c70cc6df7742053dd53a1abf7097f1564654
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-09-15 22:17:30 -0300

    exer

---
+++ Documents/NetBeansProjects/Treino/src/main/java/barbara/treino/Exercicio01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package barbara.treino;
+
+/**
+ *
+ * @author ADM
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        int[] numeros={10,10,10,10,10,10};
+        int soma=0;
+        for(int i=0; i<numeros.length;i++){
+           soma+=numeros[i]; 
+        }
+        System.out.println(soma);
+    }
+}
